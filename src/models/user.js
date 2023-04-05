598f55372f33666ea39e0962a235f79e392bcdd4
haydarselmi <haydarselmi55@gmail.com>
2023-04-05 16:28:49 +0200

    Début du projet Ajout de la base du model

---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Creates The user Schema in the MangoDB chatbot database
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
